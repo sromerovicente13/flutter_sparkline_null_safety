781b198550db75536d373db978cacefc361c5531
Drew Heavner <veedubusc@gmail.com>
2021-03-12 14:32:14 -0500

    Fixed changed api in InlineSpan

--- lib/src/sparkline.dart
+++ lib/src/sparkline.dart
@@ -308,7 +308,7 @@ class _SparklinePainter extends CustomPainter {
     }
 
     if (enableGridLines) {
-      width = size.width - gridLineTextPainters[0].text.text.length * 6;
+      width = size.width - gridLineTextPainters[0].text.toPlainText().length * 6;
       Paint gridPaint = new Paint()
         ..color = gridLineColor
         ..strokeWidth = gridLineWidth;
